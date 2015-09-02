67d7117c3f02909d24d745e3de84ba62c06d6c1f
David Aghassi <aghassi@case.edu>
2015-09-02 19:40:58 -0400

    Added main method and outlined functionality
    - Added functions to processes.c
    - Outlined fork loop

--- Homeworks/Assignment_1/assignment1.c
+++ Homeworks/Assignment_1/assignment1.c
@@ -1,14 +1,37 @@
-#include <string.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <processes.h>
 
 int main(int argc, char const *argv[])
 {
-	// Define a fork count so we can iterate
-	/*
-	Loop over all the processes so we know when all the child processes are over
-	On each loop we can iterate / check the fork count to see what child we are dealing with
-	Move child methods to separate file for neatness
-	*/
-	return 0;
+    // Define a fork count so we can iterate
+    int forkCount = 4;
+    int loopCount = 1
+
+    for (loopCount; loopCount < forkCount; loopCount++) {
+        switch(loopCount) {
+            case loopCount == 1:
+                runProcess1();
+            case loopCount == 2:
+                runProcess2();
+            case loopCount == 3:
+                runProcess3();
+            case loopCount == 4:
+                runProcess4();
+        }
+    }
+
+    wait();
+    /*
+    Loop over all the processes so we know when all the child processes are over
+    On each loop we can iterate / check the fork count to see what child we are dealing with
+    Move child methods to separate file for neatness
+
+    On each loop call method for child process
+    At beginning of method call check against child processes fork count so we know 
+    whether or not the code should be executed
+
+    USE EXECVP since exec isn't really a thing anymore.
+    */
+    return 0;
 }
---
+++ Homeworks/Assignment_1/processes.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void runProcess1() {
+
+}
+
+void runProcess2() {
+
+}
+
+void runProcess3() {
+
+}
+
+void runProcess4() {
+    
+}
