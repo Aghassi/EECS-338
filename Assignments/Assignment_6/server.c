87007a396c9e64e792513bd13f530107337b9c34
David Aghassi <dsa28@eecslinab3.(none)>
2015-11-21 15:39:59 -0500

    Corrected seg faults by properly passing integer pointers

--- Assignments/Assignment_6/server.c
+++ Assignments/Assignment_6/server.c
@@ -9,15 +9,20 @@ int cookieCount = 20;
 // if this is set to 0, we set it back to 2
 int tinaCount = 2;
 
-int * get_cookie_1_svc(struct input *argp, struct svc_req *rqstp) {
+int outOfCookies = -2;
+int judyWaits = -1;
+int giveCookie = 1;
+
+int * get_cookie_1_svc(input *argp, struct svc_req *rqstp) {
     // Make sure that Tina gets her priority
-    if(&argp->name == 'J') {
+    if(argp->name == 'J') {
         if (tinaCount == 0 ) {
             return decrementCookie();
         }
         else if(tinaCount > 0) {
+            printf("Judy must wait for Tina to get 2 cookies");
             // Judy has to wait for Tina to have at least 2
-            return (int *)-1;
+            return &judyWaits;
         }
     }
     else {
@@ -31,12 +36,12 @@ int * get_cookie_1_svc(struct input *argp, struct svc_req *rqstp) {
 int * decrementCookie() {
     if(cookieCount == 0) {
         // We are all out of cookies
-        return (int *)-2;
+        return &outOfCookies;
     }
     else {
         // Hand out a cookie
         printf("A cookie has been given out. There are now %i cookies", cookieCount);
         cookieCount--;
-        return (int *)1;
+        return &giveCookie;
     }
 }
